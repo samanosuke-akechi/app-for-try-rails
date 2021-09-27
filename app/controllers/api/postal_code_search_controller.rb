c790353f494051bd3e6bd028191d7d6099b52714
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-09-27 15:06:36 +0900

    postal_code_searchコントローラー生成

---
+++ app/controllers/api/postal_code_search_controller.rb
@@ -0,0 +1,4 @@
+class Api::PostalCodeSearchController < ApplicationController
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "posts#index"
   resources :posts, only: [:index]
 
+  namespace :api do
+    get 'postal_code_search/search'
+  end
+
   # コマンドでcontrollerファイルをディレクトリ階層いかに生成した場合、以下のようにルーティングが自動記述される。
   namespace :hoge do # namespace ディレクトリ名でURLを"/ディレクトリ名/コントローラー名", 見に行くコントローラーも"/ディレクトリ名/コントローラー名"になる。実装によってscopeやscope moduleなどを使い分けるとよい
     # get 'posts/index'
