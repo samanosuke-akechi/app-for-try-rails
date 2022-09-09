4e516006430d65507f154e61ad5800d80b5c5f43
samanosuke-akechi <toshi.kensei@gmail.com>
2022-09-09 13:00:02 +0900

    add validation test of comic model
    command
    rails g rspec:model comic

---
+++ spec/models/comic_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Comic, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comic = Comic.new(title: 'タイトル', author: '著者')
+  end
+
+  describe 'コミック登録' do
+    context 'コミックが登録できる時' do
+      it 'すべての情報があれば登録できる' do
+        expect(@comic).to be_valid
+      end
+    end
+
+    context 'コミックが登録できない時' do
+      it 'titleが空だと登録できない' do
+        @comic.title = ''
+        @comic.valid?
+        expect(@comic.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'authorが空だと登録できない' do
+        @comic.author = ''
+        @comic.valid?
+        expect(@comic.errors.full_messages).to include("Author can't be blank")
+      end
+    end
+  end
+end
