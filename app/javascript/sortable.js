7bd4c6a49b92fd295493cbb64aa37732398230a5
samanosuke-akechi <toshi.kensei@gmail.com>
2022-11-23 15:53:31 +0900

    Update sortable.js
    取得する要素のないページのときにその先の処理をしないようにして、エラーを回避。

--- app/javascript/sortable.js
+++ app/javascript/sortable.js
@@ -2,6 +2,10 @@ import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
 
 window.onload = function() {
   const el = document.getElementById('sortable');
+  if (el === null) {
+    return;
+  }
+
   const sortable = Sortable.create(el, {
     handle: '.sort-handle',
     onUpdate: function(e) {
