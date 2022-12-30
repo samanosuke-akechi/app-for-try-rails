442ed319d5777ba34b9ba80712e070a232a1b7e0
samanosuke-akechi <toshi.kensei@gmail.com>
2022-12-30 17:37:46 +0900

    Install and Setup stimulus-rails
    add `stimulus-rails` in Gemfile. run `bundle install` and `./bin/rails stimulus:install`.

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,6 @@ pin 'sortable'
 pin 'jquery', to: 'https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js'
 pin 'slick-carousel', to: 'https://ga.jspm.io/npm:slick-carousel@1.8.1/slick/slick.js'
 pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js'
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
