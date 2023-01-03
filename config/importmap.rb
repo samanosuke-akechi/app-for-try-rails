d7bbd3f7354a6899e7cd294490448d02ab272784
samanosuke-akechi <toshi.kensei@gmail.com>
2023-01-03 16:47:23 +0900

    nested form for tweet_tag

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,4 @@ pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'stimulus-rails-nested-form', to: 'https://ga.jspm.io/npm:stimulus-rails-nested-form@4.0.0/dist/stimulus-rails-nested-form.es.js'
