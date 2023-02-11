2b6daf88aa20037b0b927cb11360bf458d389094
samanosuke-akechi <toshi.kensei@gmail.com>
2023-02-12 00:33:55 +0900

    little fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(:sort_priority)
     @posts_limit10 = Post.all.limit(10)
-    logger.info "Postのタイトル: #{@posts.first.title}"
   end
 
   def google_map; end
