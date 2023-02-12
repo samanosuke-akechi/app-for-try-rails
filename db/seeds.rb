eace32a3a192539b87019d59eb05865e9b63d130
samanosuke-akechi <toshi.kensei@gmail.com>
2023-02-12 12:51:34 +0900

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,4 +26,6 @@ file_path = 'spec/fixtures/300x300.png'
   )
   tweet_image = TweetImage.new(tweet: tweet)
   tweet_image.storage_file.attach(io: File.open(file_path), filename: File.basename(file_path))
+  tweet_image.save
+  num += 1
 end
