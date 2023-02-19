98b19c7390566bbabc24b184c7c46cdbe45928b0
samanosuke-akechi <toshi.kensei@gmail.com>
2023-02-20 00:03:35 +0900

    Update areas_controller.rb

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,5 +1,5 @@
 class AreasController < ApplicationController
-  before_action :set_area, only: %i[ show edit update destroy ]
+  before_action :set_area, only: %i[show edit update destroy]
 
   # GET /areas or /areas.json
   def index
@@ -7,8 +7,7 @@ class AreasController < ApplicationController
   end
 
   # GET /areas/1 or /areas/1.json
-  def show
-  end
+  def show; end
 
   # GET /areas/new
   def new
@@ -16,8 +15,7 @@ class AreasController < ApplicationController
   end
 
   # GET /areas/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /areas or /areas.json
   def create
@@ -25,7 +23,7 @@ class AreasController < ApplicationController
 
     respond_to do |format|
       if @area.save
-        format.html { redirect_to area_url(@area), notice: "Area was successfully created." }
+        format.html { redirect_to area_url(@area), notice: 'Area was successfully created.' }
         format.json { render :show, status: :created, location: @area }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +36,7 @@ class AreasController < ApplicationController
   def update
     respond_to do |format|
       if @area.update(area_params)
-        format.html { redirect_to area_url(@area), notice: "Area was successfully updated." }
+        format.html { redirect_to area_url(@area), notice: 'Area was successfully updated.' }
         format.json { render :show, status: :ok, location: @area }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +50,20 @@ class AreasController < ApplicationController
     @area.destroy
 
     respond_to do |format|
-      format.html { redirect_to areas_url, notice: "Area was successfully destroyed." }
+      format.html { redirect_to areas_url, notice: 'Area was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_area
-      @area = Area.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def area_params
-      params.require(:area).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_area
+    @area = Area.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def area_params
+    params.require(:area).permit(:name)
+  end
 end
