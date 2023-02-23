1d4489852806ea2f815340084bf00028128bd43b
samanosuke-akechi <toshi.kensei@gmail.com>
2023-02-23 22:50:01 +0900

    refactor cdn url

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,21 @@ Rails.application.routes.draw do
   end
 
   resources :tweets, only: [:index, :new, :create, :edit, :update]
+
+  # Bootstrap JavaScript Bundle with Popper
+  direct :bootstrap_javascript do
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+  end
+  # Bootstrap Icons
+  direct :bootstrap_icons_css do
+    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css'
+  end
+
+  # Slick(JavaScriptはimportmapで導入)
+  direct :slick_css do
+    'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css'
+  end
+  direct :slick_theme_css do
+    'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css'
+  end
 end
