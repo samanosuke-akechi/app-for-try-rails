b9f6b4d0c928b0bd3533bc06227d469728b100d3
samanosuke-akechi <toshi.kensei@gmail.com>
2023-02-23 18:06:16 +0900

    Update comic_genre_form.rb

--- app/models/comic_genre_form.rb
+++ app/models/comic_genre_form.rb
@@ -10,19 +10,21 @@ class ComicGenreForm
     validates :comic
   end
 
+  # TODO: 以下だと空のgenresテーブルのレコードが生成されるので対応する
   def initialize(params = [], comic_id: nil)
-    if params.present? && comic_id.nil?
+    if params.present? && comic_id.nil? # create
       self.comic = Comic.new(params['comic'])
       self.genres = params['genres'].map { |genre| Genre.new(genre) }
-    else
+    else # new, edit, update
       self.comic = Comic.find_or_initialize_by(id: comic_id)
-      self.genres = self.comic.persisted? ? self.comic.genres : GENRE_NUM.times.map { Genre.new }
+      self.genres = comic.persisted? ? comic.genres : GENRE_NUM.times.map { Genre.new }
     end
   end
 
   def save
+    result = true
     ActiveRecord::Base.transaction do
-      result = comic.save
+      result &= comic.save
       return false unless result
 
       genres.each do |genre|
@@ -32,20 +34,21 @@ class ComicGenreForm
 
       # 意図的にrollback返さないとバリデーションに引っかかったところ以外が保存される
       raise ActiveRecord::Rollback unless result
-      result
     end
+    result
   end
 
   def update(params)
+    result = true
     ActiveRecord::Base.transaction do
-      result = comic.update(params['comic'])
+      result &= comic.update(params['comic'])
 
       genres.each do |genre|
         result &= genre.update(params['genres'][genre.id.to_s])
       end
 
       raise ActiveRecord::Rollback unless result
-      result
     end
+    result
   end
 end
