e42e6cfdc5c2bc0ca3de11ec86b5995cf551018f
samanosuke-akechi <toshi.kensei@gmail.com>
2023-03-09 00:54:10 +0900

    Create Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_124519) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_151546) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -75,6 +75,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_124519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sort_priority"
+  end
+
+  create_table "tags", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tweet_images", charset: "utf8", force: :cascade do |t|
