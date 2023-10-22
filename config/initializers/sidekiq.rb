f31e08c499cafcbf11bfae981a6891c1281ac008
samanosuke-akechi <toshi.kensei@gmail.com>
2023-10-23 00:28:48 +0900

    fix redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
 Sidekiq.configure_server do |config|
-  # config.redis = { url: "redis://#{ENV.fetch('REDIS_SERVER')}" }
-  config.redis = { url: ENV.fetch('REDIS_SERVER', 'redis://redis:6379') }
+  config.redis = { url: "redis://#{ENV.fetch('REDIS_SERVER', 'redis:6379')}" }
 end
 
 Sidekiq.configure_client do |config|
-  # config.redis = { url: "redis://#{ENV.fetch('REDIS_SERVER')}" }
-  config.redis = { url: ENV.fetch('REDIS_SERVER', 'redis://redis:6379') }
+  config.redis = { url: "redis://#{ENV.fetch('REDIS_SERVER', 'redis:6379')}" }
 end
